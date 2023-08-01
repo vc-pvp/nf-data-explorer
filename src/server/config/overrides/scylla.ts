5f3bb85c424c68f937a163de85681a176fa66e04
Shawn Gillam-Wright <shawn.gillam-wright@redspace.com>
2023-08-01 16:43:55 -0300

    Added scylla overrides

---
+++ src/server/config/overrides/scylla.ts
@@ -0,0 +1,5 @@
+export const SUPPORTED_DATASTORE_TYPES = ['cassandra'];
+export const DISCOVERY_PROVIDER = 'EnvironmentDiscoveryProvider';
+export const CASSANDRA_BASE_AUTH_PROVIDER_USERNAME = process.env.CASSANDRA_USERNAME;
+export const CASSANDRA_BASE_AUTH_PROVIDER_PASSWORD = process.env.CASSANDRA_PASSWORD;
+export const CASSANDRA_PORT = 19042;
