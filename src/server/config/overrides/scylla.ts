0d572ec7258549369301a5f0870ec20a1b1ca634
Shawn Gillam-Wright <shawn.gillam-wright@redspace.com>
2024-01-09 15:30:32 -0400

    Add override for configuration

--- src/server/config/overrides/scylla.ts
+++ src/server/config/overrides/scylla.ts
@@ -1,6 +1,7 @@
 export const SUPPORTED_DATASTORE_TYPES = ['cassandra'];
 export const DISCOVERY_PROVIDER = 'EnvironmentDiscoveryProvider';
 export const CASSANDRA_PORT = 9042;
+export const CASSANDRA_CLIENT_OPTIONS_PROVIDER = "EC2CassandraClientOptionsProvider"
 export const CASSANDRA_BASE_AUTH_PROVIDER_USERNAME = undefined;
 export const CASSANDRA_BASE_AUTH_PROVIDER_PASSWORD = undefined;
 export const ENVIRONMENTS = [process.env.ENVIRONMENT];
